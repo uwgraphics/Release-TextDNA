42a3cb2900740b47e086da7c1d3439ab5a5194e6
Deidre Stuffer <stuffer@cs.wisc.edu>
2016-09-08 14:54:52 -0500

    corrected arg parse description for mode

--- textDNACSVGenerator.py
+++ textDNACSVGenerator.py
@@ -14,7 +14,7 @@ __author__ = 'wintere'
 parser = argparse.ArgumentParser(description='textDNAGenerator <corpus_path (folder of texts OR folder of folders of texts)> <output_dir> <mode> (ngram or word_sequence)')
 parser.add_argument('corpus_path', help='path to corpus to tag relative to the location of this script')
 parser.add_argument('output_dir', help='path to output folder relative to the location of this script')
-parser.add_argument('mode', help='flag for including punctuation characters in ngrams (options: ngram OR word_sequence',)
+parser.add_argument('mode', help='flag for indicating whether the dataset is n-gram or raw text (options: ngram OR word_sequence')
 parser.add_argument('--folder_sequences', help='treats named subfolders rather than individual text files as sequences. for use with ngram mode ONLY.', action="store_true")
 
 
